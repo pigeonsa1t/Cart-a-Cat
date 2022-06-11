485067f559ff7f934cffd3182257af1028f9b33a
pigeonsa1t <100164686+pigeonsa1t@users.noreply.github.com>
2022-06-11 11:55:27 -0400

    initial push - psuedo code, updated typo ^Kittens

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// we need an array of cat images to display for the user
+
+// const catImages = [
+//   {
+//     title: 'Cat',
+//     path: 'images/cat.png',
+//     width: 50,
+//     height: 50,
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+//     // img: 'images/cat.png',
+
+// we need an array of kitten images to display for the user 
+
+// const kittenImages = [
+//   {
+//     title: 'Kitten',
+//     path: 'images/kitten.png',
+
+
+
+// we need a function to allow the user to login
+
+// const login = () => {
+//   // login
+//   // const username = 'admin';
+//   // const password = 'admin';
+//   // const email = 'admin@admin.com';
+
+
+// we need a function for the cart 
+
+// const cart = (cart) => {
+//   // cart
+//   // const cart = [
+
+//   //   {
+//   //     id: kitten-1,
+//   //     name: 'Jane',
+//          img: 'images/kitten.png',
+//          type: ''
+//   //     quantity: '1',
+
+
+// we need a cat page and a kitten page 
