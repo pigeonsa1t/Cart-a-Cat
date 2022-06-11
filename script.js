16c43e7cf27c0402bbda7f5abc34b80dd01ee942
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-06-11 14:24:22 -0400

    added a script folder and added javascript for the menu-bar

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+});
