aa611a0604b1a1e465d79d6305be5a4ccacc37cf
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-06-11 15:29:04 -0400

    added window.omscroll

--- script/script.js
+++ script/script.js
@@ -6,3 +6,8 @@ menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
+
+window.onscroll =() =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active'); 
+}
