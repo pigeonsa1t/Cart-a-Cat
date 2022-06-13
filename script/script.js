d4bb58016d7550faa155177507464eb868aaad2d
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-06-12 21:54:43 -0400

    added if else statement for window.onscroll and added css for header-2

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2')
 
 
 menu.addEventListener('click', () =>{
@@ -10,4 +11,10 @@ menu.addEventListener('click', () =>{
 window.onscroll =() =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active'); 
+
+    if(window.scrollY >150){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
 }
