0beb9864b6c2e96c3f6a77a00581efe1ddef01ca
pigeonsa1t <100164686+pigeonsa1t@users.noreply.github.com>
2022-06-14 19:40:16 -0400

    node.js npm inquirer all set up

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+console.log(inquirer);
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
